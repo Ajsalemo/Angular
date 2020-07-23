4a1cfd2d80cbfcd3172cbe5ef8fde3a0a8be0b18
Anthony Salemo (Insight Global) <ansalemo@microsoft.com>
2020-07-23 12:15:44 -0400

    able to authenticate against passport - very early stages/testing

--- express/app.js
+++ express/app.js
@@ -4,7 +4,9 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
-const { auth, isLoggedIn } = require("../passport-auth");
+// const { auth, isLoggedIn } = require("../passport-auth");
+// Set the stategy to 'Local'
+const LocalStrategy = require("passport-local").Strategy;
 
 // Initialize express
 const app = new express();
@@ -12,19 +14,47 @@ const app = new express();
 // Set the port
 const port = process.env.PORT || 3000;
 
+// Currently the username and password is hardcoded
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    if (username === "admin" && password === "admin@admin.com") {
+      return done(null, username);
+    } else {
+      return done("unauthorized access", false);
+    }
+  })
+);
+
+// Serialize and deserialize the user
+passport.serializeUser(function (user, done) {
+  if (user) done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
 app.use(session({ secret: "anything", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-// Check the authentication status
-app.get("/check_auth", isLoggedIn, (req, res) => {
-  res.json("data is");
-});
+// Function to process authentication
+const auth = () => {
+  return (req, res, next) => {
+    passport.authenticate("local", (error, user, info) => {
+      if (error) res.status(400).json({ statusCode: 200, message: error });
+      req.login(user, function (error) {
+        if (error) return next(error);
+        res.status(200).json({ success: "You are now logged in" });
+      });
+    })(req, res, next);
+  };
+};
 
 app.post("/authenticate", auth(), (req, res) => {
-  res.status(200).json({ statusCode: 200, user: req.user });
+  res.status(200);
 });
 
 app.listen(port, () => {
