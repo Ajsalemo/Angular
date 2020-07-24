d84f5156155eb6781da6c810843e189491d1e2fb
Anthony Salemo <ajssalemo@gmail.com>
2020-07-24 09:55:59 -0400

    merging changes

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login-component',
@@ -8,6 +10,10 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent {
+  nameField: string = '';
+  emailField: string = '';
+  constructor(private authService: AuthService, private router: Router) {}
+
   loginGroupOne = new FormGroup({
     name: new FormControl('', [
       Validators.required,
@@ -17,18 +23,28 @@ export class LoginComponent {
   });
 
   loginGroupTwo = new FormGroup({
-    email: new FormControl('', [
+    password: new FormControl('', [
       Validators.required,
       Validators.minLength(1),
       Validators.maxLength(255),
     ]),
   });
 
-  submitLoginFormOne() {
-    console.log(this.loginGroupOne.value)
+  // These two functions are for the step-form
+  submitLoginFormOne(data: { name: string }) {
+    this.nameField = data.name;
   }
 
-  submitLoginFormTwo() {
-    console.log(this.loginGroupTwo.value)
+  // This function takes the 'nameField' value and sets it with the value of 'emailField' 
+  // These values are supplied to the arguemnts of 'validate' to be proxied to Passport.js
+  submitLoginFormTwo(data: { password: string }) {
+    this.emailField = data.password;
+    this.authService
+      .validate(this.nameField, this.emailField)
+      .then((response) => {
+        console.log(response);
+        this.authService.setUserInfo({ user: response['user'] });
+        this.router.navigate(['main']);
+      });
   }
 }
--- angular/src/services/auth.service.ts
+++ angular/src/services/auth.service.ts
@@ -1,27 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   loggedInUserInfo: {};
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  isAuthenticated() : Boolean {
-    let userData = localStorage.getItem('userInfo')
-    if(userData && JSON.parse(userData)){
+  isAuthenticated(): Boolean {
+    let userData = localStorage.getItem('user');
+    if (userData && JSON.parse(userData)) {
       return true;
     }
     return false;
   }
 
-  setUserInfo(user: object){
-    localStorage.setItem('userInfo', JSON.stringify(user));
+  setUserInfo(user: object) {
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
-  validate(email: string, password: string) {
-    return this.http.post('/api/authenticate', {'username' : email, 'password' : password}).toPromise()
+  validate(username: string, password: string) {
+    return this.http
+      .post('/api/authenticate', { username: username, password: password })
+      .toPromise();
   }
 }
