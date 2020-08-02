af7593021c43cf88bb78810ebd200368cc656ba3
Anthony Salemo <ajssalemo@gmail.com>
2020-08-02 19:09:03 -0400

    added a script in the Node.js dockerfile to wait for the Postgres service to be fully up before initializing the application

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: "",
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
-    host: "127.0.0.1",
+    host: "",
     dialect: "postgres",
   },
   production: {
-    username: process.env.POSTGRES_USERNAME,
+    username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: "",
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
   },
 };
