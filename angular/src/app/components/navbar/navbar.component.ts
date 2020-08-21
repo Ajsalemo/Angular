65319cda906f8df378cd942dba8a3457ecd74ac0
Anthony Salemo <ajssalemo@gmail.com>
2020-08-21 11:17:49 -0400

    transitions now apply to all elements that can be toggles for account preferences

--- angular/src/app/components/navbar/navbar.component.ts
+++ angular/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { style, animate, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'component-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        // :leave is alias to '* => void'
+        animate(500, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent {
-  @Input() showLinks: boolean;
+  @Input() parentIsLinks: boolean;
+  @Input() parentIsSearch: boolean;
+  @Input() parentIsWeather: boolean;
 }
