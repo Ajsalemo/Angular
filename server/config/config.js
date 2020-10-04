dbfae5016ede40ac0fccc551d889fa704e5e71a9
Anthony Salemo <ajssalemo@gmail.com>
2020-10-03 20:22:15 -0400

    changed conditional auth logic

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST, //""
+    host: "", //process.env.POSTGRES_HOST, 
     dialect: "postgres",
   },
   test: {
