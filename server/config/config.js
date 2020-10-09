bafaa94a6c9e755ed1c5d96efdba6ac1cfdc1c17
Anthony Salemo <ajssalemo@gmail.com>
2020-10-09 13:26:49 -0400

    connected sequelize to azure postgres for db

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,7 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST, //"" <- Use this value when running locally
+    port: process.env.POSTGRES_PORT,
     dialect: "postgres",
   },
   test: {
@@ -14,10 +15,11 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
+    username: process.env.AZURE_POSTGRES_USER,
+    password: process.env.AZURE_POSTGRES_PASSWORD,
+    database: process.env.AZURE_POSTGRES_DB,
+    host: process.env.AZURE_POSTGRES_HOST,
+    port: process.env.AZURE_POSTGRES_PORT,
     dialect: "postgres",
   },
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,32 +1,46 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+// const env = process.env.NODE_ENV || "development";
+// const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    `postgresql://${process.env.AZURE_POSTGRES_USER}:${process.env.AZURE_POSTGRES_PASSWORD}@${process.env.AZURE_POSTGRES_HOST}:${process.env.AZURE_POSTGRES_PORT}/${process.env.AZURE_POSTGRES_DB}`,
+    {
+      dialect: "postgres",
+    }
+  );
 } else {
-  sequelize = new Sequelize(`postgresql://${config.username}:${config.password}@${config.host}:5432/${config.database}`)
+  sequelize = new Sequelize(
+    `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+    {
+      dialect: "postgres",
+    }
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
