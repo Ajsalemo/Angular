1d39436503502b2665eab7b75d5837fd3b2a5ef0
Anthony Salemo <ajssalemo@gmail.com>
2020-10-09 13:57:26 -0400

    edited models/index.js to re-add the postgres host for development

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST, //"" <- Use this value when running locally
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     dialect: "postgres",
   },
